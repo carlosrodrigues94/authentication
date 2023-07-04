bac87b58bc62d4d48205c848af413d51d70f1456
carlosrodrigues94 <carlos.1994hrs@gmail.com>
2023-07-04 13:15:24 -0300

    ci: fix lambda build

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+import { APIGatewayEvent, Context } from "aws-lambda";
 import { verify, decode, sign } from "jsonwebtoken";
 
-export const generateToken = async (event) => {
-  console.log(event);
+export const generateToken = async (
+  event: APIGatewayEvent,
+  context: Context
+) => {
+  console.log({ event, context });
   return sign({ userID: "user-id" }, "secret");
 };
